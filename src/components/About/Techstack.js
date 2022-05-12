855fea1f30c81ed6c71a23e21f08dcb69e696719
lingeshwaran <rishilee03@gmail.com>
2022-05-12 21:32:54 +0530

    removed

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+} from "react-icons/di";
+import {
+  SiMaterialui,
+  SiFirebase,
+  SiNextdotjs,
+  SiExpress,
+} from "react-icons/si";
+import { FaBootstrap ,FaFontAwesomeFlag} from 'react-icons/fa';
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress/>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaFontAwesomeFlag />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMaterialui />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaBootstrap />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
