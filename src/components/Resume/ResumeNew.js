f05f485c8ae391edc7d64ef12a9728baa5d85567
lingeshwaran <rishilee03@gmail.com>
2022-07-01 07:32:30 +0530

    removed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -12,7 +12,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/Lingeshwaranlee/resume/blob/main/LINGESH-RESUME%20(1).pdf";
+  "https://github.com/Lingeshwaranlee/lingesh-portfolio/blob/master/src/Assets/lingeshwaran%20pdf.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
